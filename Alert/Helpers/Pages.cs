1c487ddd28d5499f49da31c165e6b361b9722d67
TheHZ <dddd73@yandex.ru>
2017-12-01 00:38:25 +0400

    initial commit

---
+++ Alert/Helpers/Pages.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace Alert
+{
+    class Pages
+    {
+        private IWebDriver driver;
+        private By inputAlertTab = By.XPath("//a[contains(text(),'Input Alert')]");
+        private By alertButton = By.XPath(
+            "//button[contains(text(), 'Click the button to demonstrate the Input box')]");
+        private By checkText = By.Id("demo");
+        private By imageAlert = By.XPath("//img[@src=\"images/simple_alert.jpg\"]");
+
+        public Pages(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenWindowEnterTextAlert()
+        {
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(inputAlertTab));
+            driver.FindElement(inputAlertTab).Click();
+            driver.SwitchTo().Frame(1);
+            driver.FindElement(alertButton).Click();
+        }
+
+        public void OpenAlertPage()
+        {
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(imageAlert));
+            driver.FindElement(imageAlert).Click();
+        }
+
+        public void SetTextInAlert(string text)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.AlertIsPresent());
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.SendKeys(text);
+            alert.Accept();
+        }
+
+        public bool CheckInputText(string text)
+        {
+            return driver.FindElement(checkText).Text.Contains(text);
+        }
+    }
+}
---
+++ Alert/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace Alert
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            string directoryForChromeDriver = Directory.GetCurrentDirectory();
+            driver = new ChromeDriver(directoryForChromeDriver);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestAlert()
+        {
+            driver.Navigate().GoToUrl(@"http://way2automation.com/way2auto_jquery/alert.php");
+
+            string login = "TheHZ";
+            string password = "Wizard73";
+            var loginForm = new LoginForm(driver);
+            loginForm.Login(login, password);
+
+            var page = new Pages(driver);
+            page.OpenAlertPage();
+            page.OpenWindowEnterTextAlert();
+
+            string inputInAlertText = "Текст для ввода и проверки";
+            page.SetTextInAlert(inputInAlertText);
+            Assert.True(page.CheckInputText(inputInAlertText));
+        }
+    }
+}
+
